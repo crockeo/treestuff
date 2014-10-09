c1bfec58623f5307a2d1fa1da8f0b531ea212046
Crockeo <crockeo@gmail.com>
2014-10-09 15:03:37 -0400

    Pointed the python implementation to the global dictionary.

--- trie_in_py/trie.py
+++ trie_in_py/trie.py
@@ -1,12 +1,15 @@
 import string
 import datrie
+
+dictionary = '../dictionaries/dictionary.txt'
+
 trie = datrie.Trie(string.ascii_lowercase)
 
-with open('dictionary.txt', 'r') as f:
+with open(dictionary, 'r') as f:
     for line in f.readlines():
         trie[unicode(line.strip())] = True
 
-with open('dictionary.txt', 'r') as f:
+with open(dictionary, 'r') as f:
     exists = 0
     for line in f.readlines():
         if unicode(line.strip()) in trie:
