b664861b388fe24956f712e56d6a0fa3fa9528e0
Crockeo <crockeo@gmail.com>
2014-10-09 10:58:18 -0400

    Made the main run a test.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+--------------------
+-- Global Imports --
+import Control.Monad
+
+-------------------
+-- Local Imports --
+import Dictionary
+import TreeStuff
+
 main :: IO ()
-main = print "Nothing here."
+main = do
+  strs <- loadDefaultDict
+  let strs'  = map (reverse) strs
+      strs'' = strs ++ strs'
+  dict <- defaultDictTree
+
+  forM_ strs'' $ \s ->
+    print (s, findString dict s)
